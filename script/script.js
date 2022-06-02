b7bdf6635888e4909cd2fe92268901e2e88dd5a1
WCram <wcram@rtc.edu>
2022-06-02 10:17:36 -0700

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,32 @@
+// loops
+// conditions
+// variables
+// operators
+// input
+// output
+
+// Variables
+const constant = 0;
+var dontDoVar = 5;
+let doLet = 10;
+
+// Conditions
+if(true) {
+
+}
+else if (true) {
+
+}
+else
+{
+
+}
+
+switch("1") {
+    case "1":
+
+    break;
+    default: 
+
+    break;
+}
